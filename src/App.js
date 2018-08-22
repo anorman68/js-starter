12deea4d4703b760b56cd8eebd75fbf5cd2a98be
anorman68 <anorman68@gmail.com>
2018-08-22 10:19:17 -0400

    remove typescript support, add 'chalk', add 'commonJS' support

--- src/App.js
+++ src/App.js
@@ -1,2 +1,7 @@
-// @ts-check
-let easy = 'abc'; /*eslint-disable-line no-unused-vars*/
+var easy = 'abc'; /*eslint-disable-line no-unused-vars*/
+var chalk = require('chalk');
+
+var msg = 'Hello World';
+console.log(msg); /*eslint-disable-line no-console*/
+
+console.log(chalk.blue(msg));
